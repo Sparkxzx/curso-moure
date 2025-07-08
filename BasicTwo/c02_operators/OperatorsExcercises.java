9b14c28b6ae2ed43b3e8cc35a7aa32fda4a03d40
Spark <davis44566@gmail.com>
2025-07-07 20:32:16 -0600

    nuevos dos

--- BasicTwo/c02_operators/OperatorsExcercises.java
+++ BasicTwo/c02_operators/OperatorsExcercises.java
@@ -12,21 +12,45 @@ public class OperatorsExcercises {
             System.out.println(a / 12);
             System.out.println(a * 4);
         // 2. Crea una variable para cada tipo de operación de asignación.
+            int b = 5;
+
+            b = 6;
+
+            b += 6;
+            System.out.println(b);
+            b -= 6;
+            System.out.println(b);
+            b *= 6;
+            System.out.println(b);
+            b /= 6;
+            System.out.println(b);
+            b %= 6;
+            System.out.println(b);
 
         // 3. Imprime 3 comparaciones verdaderas con diferentes operadores de comparación.
-
+            System.out.println(a > b);
+            System.out.println(b == 0);
+            System.out.println(b < a);
         // 4. Imprime 3 comparaciones falsas con diferentes operadores de comparación.
-
+            System.out.println(a == b);
+            System.out.println(b > a);
+            System.out.println(a < b);
         // 5. Utiliza el operador lógico and.
-
-        // 6. Utiliza el operador lógico or.
-
+            System.out.println(a < b && a > b);
+            System.out.println(a == b && b == a);
+            // 6. Utiliza el operador lógico or.
+            System.out.println(a > b  || b == a);
         // 7. Combina ambos operadores lógicos.
-
+            System.out.println(b == a && a < b || b > a);
         // 8. Añade alguna negación.
-
+            System.out.println(b != a);
         // 9. Imprime 3 ejemplos de uso de operadores unarios.
-
-        // 10. Combina operadores aritméticos, de comparación y lógicos.
+            b  =  8;
+            System.out.println(++b);
+            System.out.println(--b);
+            System.out.println(+b);
+            System.out.println(-b);
+        // 10. Combina operadores aritméticos, de comparación y lógicos.Sy
+           System.out.println(a == b + 8 && b < a * 4 || a > b);
     }
 }
--- BasicTwo/c03_strings/strings.java
+++ BasicTwo/c03_strings/strings.java
@@ -1,4 +1,16 @@
 package BasicTwo.c03_strings;
 
 public class strings {
+    public static void main(String[] args){
+
+        // Cadenas de texto
+        //Declaración de texto
+        String name = "Moure";
+        var surname = new String("David");
+        // var name = "David";
+
+        //
+
+
+    }
 }
